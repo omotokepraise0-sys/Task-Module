23160890442657580f0d235046fd86f3491b99c1
Omotoke Praise <omotokepraise0@gmail.com>
2026-05-07 00:27:45 +0100

    add more words

--- Task_Module/settings.py
+++ Task_Module/settings.py
@@ -135,6 +135,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 ## This is for uploading images
 MEDIA_URL = "/media/"
